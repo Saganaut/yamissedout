e564cf9129c5f645d93bca572c8c62883de5b100
Yacin Nadji <yacin@gatech.edu>
2015-01-29 20:30:20 -0500

    wordcloud pull from DB

--- python/cloud.py
+++ python/cloud.py
@@ -6,30 +6,38 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from scipy.misc import imread
 
+import textstats
+
 def main():
   """main function for standalone usage"""
-  usage = "usage: %prog [options] words"
+  usage = "usage: %prog [options]"
   parser = OptionParser(usage=usage)
   parser.add_option('-d', '--db', default='../db/missed_connections.db',
                     help='DB file [default: %default]')
   parser.add_option('-m', '--mask', default=None, help='Image mask')
+  parser.add_option('-n', '--ngram', default=3, type='int',
+                    help='n for n-grams [default: %default]')
+  parser.add_option('-r', '--num-records', default=800, type='int',
+                    help='Max number of records [default: %default]')
 
   (options, args) = parser.parse_args()
 
-  if len(args) != 1:
+  if len(args) != 0:
       parser.print_help()
       return 2
 
-  with open(args[0]) as f:
-      words = [(' '.join(x.split()[:-1]),
-                int(x.split()[-1])) for x in f.readlines()]
-      if options.mask:
-          dickmask = imread(options.mask)
-          wordcloud = WordCloud(background_color='white', mask=dickmask)
-      else:
-          wordcloud = WordCloud()
-      wordcloud.fit_words(words)
-      wordcloud.to_file('shitdick.png')
+  results = textstats.read_from_db(options.db,
+                                   num_grams=options.ngram,
+                                   maxrecords=options.num_records)
+  words = [(' '.join(tokens), count) for (tokens, count) in results]
+  
+  if options.mask:
+    dickmask = imread(options.mask)
+    wordcloud = WordCloud(background_color='white', mask=dickmask)
+  else:
+    wordcloud = WordCloud()
+  wordcloud.fit_words(words)
+  wordcloud.to_file('shitdick.png')
 
 if __name__ == '__main__':
     sys.exit(main())
