de3041a4ba2a181d8e140e221e7c7e520336b05e
saganaut <robert.s.pienta@gmail.com>
2015-01-29 19:02:50 -0500

    dickly silhouette

--- python/scrape_mc.py
+++ python/scrape_mc.py
@@ -13,7 +13,7 @@ from urlparse import urljoin
 # The base url for craigslist in New York
 BASE_URL = 'http://atlanta.craigslist.org/search/mis'
 
-do_extract_pics = 0
+do_extract_pics = 1
 
 #viable classes
 mc_classes = set(['w4m', 'm4m', 'm4w', 'w4w', 't4m', 'm4t', 't4w', 'w4t', 't4t', 'mw4mw', 'mw4w', 'mw4m', 'w4mw', 'm4mw', 'w4ww', 'm4mm', 'mm4m', 'ww4w', 'ww4m', 'mm4w', 'm4ww', 'w4mm', 't4mw', 'mw4t'])
