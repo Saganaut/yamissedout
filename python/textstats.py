8b558d5ab11662200cf52f966ce8112472e13d46
Casey Battaglino <cjbattagl@gmail.com>
2015-02-05 21:32:26 -0500

    added parse options to textstats

--- python/textstats.py
+++ python/textstats.py
@@ -3,9 +3,10 @@ import os
 import sqlite3
 import re
 import nltk
+from optparse import OptionParser
 from nltk.collocations import *
 
-def read_from_db(db_name, num_grams=3, maxrecords=800):
+def read_from_db(db_name, num_grams=3, maxrecords=40):
   conn = sqlite3.connect(db_name)
   cursor = conn.cursor()
   cursor.execute("SELECT body FROM missed_connections")
@@ -21,7 +22,7 @@ def read_from_db(db_name, num_grams=3, maxrecords=800):
     body = cursor.fetchone()
 
   conn.close()
-  # entity_extraction(line)
+  #entity_extraction(line)
 
   tokens = line.split()
   bigram_measures = nltk.collocations.BigramAssocMeasures()
@@ -33,6 +34,7 @@ def read_from_db(db_name, num_grams=3, maxrecords=800):
   return fdist.most_common(maxrecords)
 
 def entity_extraction(document):
+  #if this crashes run 'python -m nltk.downloader all'
   sentences = nltk.sent_tokenize(document)
   sentences = [nltk.word_tokenize(sent) for sent in sentences]
   sentences = [nltk.pos_tag(sent) for sent in sentences]
@@ -42,12 +44,30 @@ def entity_extraction(document):
   print(result)
   exit()
 
-db_path = '../db/missed_connections.db'
-if __name__ == '__main__':
+def main():
+  """main function for standalone usage"""
+  usage = "usage: %prog [options] input"
+  parser = OptionParser(usage=usage)
+  parser.add_option('-d', '--db-name', default='../db/missed_connections.db', type='string',
+                    help='Database name BITCH [default: %default]')
+  parser.add_option('-n', '--num-grams', default=3, type='int',
+                    help='Number of grams for the n-gram BITCH [default: %default]')
+  parser.add_option('-m', '--max-records', default=40, type='int',
+                    help='Max number of records to parse from the db BITCH [default: %default]')
+
+  (options, args) = parser.parse_args()
+
+  if (len(args) != 0) :#or (options.num_pages <= 0) or (options.extract_pics not in (0,1)) :
+    parser.print_help()
+    return 2
+
+  db_path = options.db_name
   if not os.path.isfile(db_path): 
     print "No missed connections database at " + db_path + "... Run scrape_mc.py first."
 
-  for k,v in read_from_db(db_path):
+  for k,v in read_from_db(db_name=options.db_name, num_grams=options.num_grams, maxrecords=options.max_records):
     print ' '.join([str(i) for i in k]), v
 
 
+if __name__ == '__main__':
+    sys.exit(main())
