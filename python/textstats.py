24faf5f07f32fe4d6b34533aa43de1c1d3afdfb1
Casey Battaglino <cjbattagl@gmail.com>
2015-02-05 22:34:18 -0500

    example stop words and likelihood filtering code

--- python/textstats.py
+++ python/textstats.py
@@ -3,8 +3,10 @@ import os
 import sqlite3
 import re
 import nltk
+import pprint
 from optparse import OptionParser
 from nltk.collocations import *
+from nltk.corpus import stopwords
 
 def read_from_db(db_name, num_grams=3, maxrecords=40):
   conn = sqlite3.connect(db_name)
@@ -27,13 +29,37 @@ def read_from_db(db_name, num_grams=3, maxrecords=40):
   line = ''.join(lines)
   #entity_extraction(line)              
   tokens = line.split()
-  bigram_measures = nltk.collocations.BigramAssocMeasures()
-  finder = BigramCollocationFinder.from_words(tokens)
-  finder.apply_freq_filter(3)
+
+  ignored_words = stopwords.words('english')
+  word_filter = lambda w: len(w) < 3 or w in ignored_words
+
+  pp = pprint.PrettyPrinter(indent=4)
+
+  cf_bi = nltk.BigramCollocationFinder.from_words(tokens)
+  cf_bi.apply_freq_filter(3)
+  cf_bi.apply_word_filter(word_filter)
+  bi_scorer = nltk.BigramAssocMeasures.likelihood_ratio
+  print('Top bi-grams by likelihood, excluding stop-words:')
+  best_list = [str(' '.join(tup)) for tup in cf_bi.nbest(bi_scorer, 20)]
+  pp.pprint(best_list)
+  print('\n')
+
+  cf_tri = nltk.TrigramCollocationFinder.from_words(tokens)
+  cf_tri.apply_freq_filter(3)
+  cf_tri.apply_word_filter(word_filter)
+  tri_scorer = nltk.TrigramAssocMeasures.likelihood_ratio
+  print('Top tri-grams by likelihood, excluding stop-words:')
+  best_list = [str(' '.join(tup)) for tup in cf_tri.nbest(tri_scorer, 20)]
+  pp.pprint(best_list)
+  print('\n')
 
   bgs = nltk.ngrams(tokens,num_grams)
   fdist = nltk.FreqDist(bgs)
-  return fdist.most_common(maxrecords)
+
+  print('Top ' + str(num_grams) + '-grams by frequency:')
+  ngram_rank = fdist.most_common(maxrecords)
+  for k,v in ngram_rank:
+    print '\t', ' '.join([str(i) for i in k]), v
 
 def entity_extraction(document):
   #if this crashes run 'python -m nltk.downloader all'
@@ -66,8 +92,7 @@ def main():
   if not os.path.isfile(db_path): 
     print "No missed connections database at " + db_path + "... Run scrape_mc.py first."
   else: 
-    for k,v in read_from_db(db_name=options.db_name, num_grams=options.num_grams, maxrecords=options.max_records):
-      print ' '.join([str(i) for i in k]), v
+    read_from_db(db_name=options.db_name, num_grams=options.num_grams, maxrecords=options.max_records)
 
 
 if __name__ == '__main__':
