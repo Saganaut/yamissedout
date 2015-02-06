fb281b3f014dfc23a2597c6a051ca422f366f6ad
Casey Battaglino <cjbattagl@gmail.com>
2015-02-05 21:48:59 -0500

    textstats boundchecking

--- python/textstats.py
+++ python/textstats.py
@@ -12,19 +12,20 @@ def read_from_db(db_name, num_grams=3, maxrecords=40):
   cursor.execute("SELECT body FROM missed_connections")
   num = 0
   lines = []
-  regex = re.compile('[^a-zA-Z \']')
+  regex = re.compile('[^a-zA-Z \']')    #all symbols that will be retained
+
   body = cursor.fetchone()
   while body:
     stmt = body[0].strip()
     stmt = stmt.lower()                 #convert all words to lower-case
-    stmt = regex.sub(' ', stmt)          #remove symbols
+    stmt = regex.sub(' ', stmt)         #remove symbols
     lines.append(stmt)             
     body = cursor.fetchone()
 
   conn.close()
-  
+
   line = ''.join(lines)
-  #entity_extraction(line)
+  #entity_extraction(line)              
   tokens = line.split()
   bigram_measures = nltk.collocations.BigramAssocMeasures()
   finder = BigramCollocationFinder.from_words(tokens)
@@ -57,17 +58,16 @@ def main():
                     help='Max number of records to parse from the db BITCH [default: %default]')
 
   (options, args) = parser.parse_args()
-
-  if (len(args) != 0) :#or (options.num_pages <= 0) or (options.extract_pics not in (0,1)) :
+  if (len(args) != 0) or (options.num_grams <= 1) or (options.max_records < 1) :
     parser.print_help()
     return 2
 
   db_path = options.db_name
   if not os.path.isfile(db_path): 
     print "No missed connections database at " + db_path + "... Run scrape_mc.py first."
-
-  for k,v in read_from_db(db_name=options.db_name, num_grams=options.num_grams, maxrecords=options.max_records):
-    print ' '.join([str(i) for i in k]), v
+  else: 
+    for k,v in read_from_db(db_name=options.db_name, num_grams=options.num_grams, maxrecords=options.max_records):
+      print ' '.join([str(i) for i in k]), v
 
 
 if __name__ == '__main__':
