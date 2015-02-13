0eadc37fbbf7be309bef0fac6b8bce8f1604ee13
Casey Battaglino <cjbattagl@gmail.com>
2015-02-12 21:14:45 -0500

    lol

---
+++ python/common.py
@@ -0,0 +1,5 @@
+def valid_cities():
+  return ["atlanta", "austin", "boston", "chicago", "dallas", "denver", "detroit", "houston", "lasvegas", "losangeles", "miami", "minneapolis", "newyork", "orangecounty", "philadelphia", "phoenix", "portland", "raleigh", "sacramento", "sandiego", "seattle", "sfbay", "washingtondc"]
+
+def valid_classes():
+  return set(['w4m', 'm4m', 'm4w', 'w4w', 't4m', 'm4t', 't4w', 'w4t', 't4t', 'mw4mw', 'mw4w', 'mw4m', 'w4mw', 'm4mw', 'w4ww', 'm4mm', 'mm4m', 'ww4w', 'ww4m', 'mm4w', 'm4ww', 'w4mm', 't4mw', 'mw4t'])
