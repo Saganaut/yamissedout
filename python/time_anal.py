6bcc70d61e6a9c007c3f0468f5c56732c012a355
saganaut <robert.s.pienta@gmail.com>
2015-02-26 19:16:53 -0500

    basic changes to time anal

--- python/time_anal.py
+++ python/time_anal.py
@@ -6,14 +6,19 @@ import time
 import datetime
 import csv
 
+import common
+
 db_path = '../db/missed_connections.db'
 
 
-def time_analysis(db_name, gender='m'):
+def time_analysis(db_name, gender='m', city=None):
   gender_str = {'m':'male', 'w':'female', 't':'trans'}
   conn = sqlite3.connect(db_name)
   cursor = conn.cursor()
-  cursor.execute("SELECT datetime FROM missed_connections WHERE gender = \'"+gender+"\'")  
+  if city == None or city == 'all':
+    cursor.execute("SELECT datetime FROM missed_connections WHERE gender = \'"+gender+"\'")  
+  else:
+    cursor.execute("SELECT datetime FROM missed_connections WHERE gender = \'"+gender+"\' AND city = \'"+city+"\' ")  
   rows = cursor.fetchall()
   n_peepz = len(rows)
   dates = []
@@ -28,27 +33,45 @@ def time_analysis(db_name, gender='m'):
     time_appearances[just_time]+=1
     date_appearances[date]+=1
 
-
-  with open('../web/web_data/'+gender_str[gender]+'_time.tsv', 'wb') as csvfile:
+  if city == None:
+    city = ""
+  else:
+    city+="_"
+  with open('../web/web_data/'+city+gender_str[gender]+'_time.tsv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter='\t',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(["time","frequency","count"])
-    sorted_keys = sorted(time_appearances.keys())
-    for k in sorted_keys:
-      spamwriter.writerow([k, time_appearances[k]/float(n_peepz), time_appearances[k]])
+    if n_peepz == 0:
+      spamwriter.writerow(["[]"])
+    else:
+      spamwriter.writerow(["time","frequency","count"])
+      sorted_keys = sorted(time_appearances.keys())
+      for k in sorted_keys:
+        spamwriter.writerow([k, time_appearances[k]/float(n_peepz), time_appearances[k]])
 
-  with open('../web/web_data/'+gender_str[gender]+'_days.tsv', 'wb') as csvfile:
+  with open('../web/web_data/'+city+gender_str[gender]+'_days.tsv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter='\t',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(["time","frequency","count"])
-    sorted_keys = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    for k in sorted_keys:
-      spamwriter.writerow([k, date_appearances[k]/float(n_peepz), date_appearances[k]])
+    if n_peepz == 0:
+      spamwriter.writerow(["[]"])
+    else:
+      spamwriter.writerow(["time","frequency","count"])
+      sorted_keys = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+      for k in sorted_keys:
+        spamwriter.writerow([k, date_appearances[k]/float(n_peepz), date_appearances[k]])
 
-
+def process_cities():
+  for city in common.valid_cities():
+    print city
+    time_analysis(db_path, 'm', city)
+    time_analysis(db_path, 'w', city)
+    time_analysis(db_path, 't', city)
+  time_analysis(db_path, 'm')
+  time_analysis(db_path, 'w')
+  time_analysis(db_path, 't')
 
 if __name__ == '__main__':
   if not os.path.isfile(db_path): 
     print "No missed connections database at " + db_path + "... Run scrape_mc.py first."
-  time_analysis(db_path, 'm')
-  time_analysis(db_path, 'w')
-  time_analysis(db_path, 't')
+  else:
+    process_cities()
+    time
+  
 
