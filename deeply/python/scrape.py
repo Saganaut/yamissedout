95c32ed256f1a6980e64984a13651d2ef4881fd4
Yacin Nadji <yacin@gatech.edu>
2016-02-25 17:27:49 -0500

    getcookie() and downloadfile() funcs

--- deeply/python/scrape.py
+++ deeply/python/scrape.py
@@ -6,23 +6,28 @@
 import sys
 from optparse import OptionParser
 
+from pycookiecheat import chrome_cookies
 import mechanize
 import cookielib
 from bs4 import BeautifulSoup
+import requests
 
-def login(username, password):
-    cj = cookielib.CookieJar()
-    br = mechanize.Browser()
-    br.set_cookiejar(cj)
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; CrOS x86_64 7520.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.110 Safari/537.36')]
-    br.open('http://ma.brazzers.com/access/login/')
+headers = {'User-agent': 'Mozilla/5.0 (X11; CrOS x86_64 7520.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.110 Safari/537.36'}
 
-    return br, cj
+def getcookie(cookie_path='~/.config/google-chrome-unstable/Default/Cookies', url='http://ma.brazzers.com'):
+    cookie_path = os.path.expanduser(cookie_path)
+    return chrome_cookies(url, cookie_file=cookie_path)
 
-def dumpvidandlabels(path):
-    with open(path) as f:
-        html = f.read()
+def downloadfile(url, cookie):
+    outpath = '/tmp/video.mp4'
+    r = requests.get(url, stream=True, headers=headers, cookies=cookie)
+    with open(outpath, 'wb') as out:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                out.write(chunk)
+    return outpath
 
+def dumpvidandlabels(html):
     soup = BeautifulSoup(html, 'html.parser')
 
     videoname = 'tlib_alessa_savage_ap011416_272p_650_mobile.mp4'
