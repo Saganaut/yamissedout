eab1b6c4d8ff87e416a7e617d3342c32adb5d3b6
Yacin Nadji <yacin@gatech.edu>
2016-03-11 11:59:35 -0500

    cli for choosing model; normalize data before CV
    normalizing seems to help (obv with sgd):
    
    $ time python classify_audio.py -m sgd mfcc.csv
    [ 0.59961773  0.74190845  0.74346668  0.56703019  0.7430556   0.73237989
      0.65563783  0.73409049  0.58912855  0.7433283 ]
    
    $ time python classify_audio.py -m sgd mfcc.csv
    [ 0.74432397  0.74432397  0.74432397  0.74432397  0.74432397  0.74432397
      0.74432352  0.74432352  0.74432483  0.74432614]

--- deeply/python/classify_audio.py
+++ deeply/python/classify_audio.py
@@ -3,9 +3,19 @@
 import sys
 from optparse import OptionParser
 
+import pandas as pd
 import numpy as np
 from sklearn import cross_validation
 from sklearn import svm
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import normalize
+
+models = {'svm': svm.SVC(kernel='linear', C=1),
+          'sgd': SGDClassifier(loss="hinge", penalty="l2"),
+          'rf':  RandomForestClassifier(),
+}
+
 
 def _read_mfcc_df(csvpath):
     return pd.read_csv(csvpath,
@@ -13,11 +23,12 @@ def _read_mfcc_df(csvpath):
                        sep=',',
                        names=['mfcc%d' % x for x in range(13)] + ['class'])
 
-def xfold(df, k=10):
+def xfold(df, classifier, k=10):
     X = df[['mfcc%d' % x for x in range(13)]]
+    normalize(X, copy=False)
     y = df['class']
 
-    clf = svm.SVC(kernel='linear', C=1)
+    clf = models[classifier]
     scores = cross_validation.cross_val_score(clf, X, y, cv=k)
     return scores
 
@@ -25,15 +36,17 @@ def main():
     """main function for standalone usage"""
     usage = "usage: %prog [options] mfcc.csv"
     parser = OptionParser(usage=usage)
+    parser.add_option('-m', '--model', default='svm',
+                      help='Model to use. One of: %s' % ', '.join(models.keys()))
 
     (options, args) = parser.parse_args()
 
-    if len(args) != 1:
+    if len(args) != 1 or options.model not in models:
         parser.print_help()
         return 2
 
     # do stuff
-    print(xfold(_read_mfcc_df(args[0])))
+    print(xfold(_read_mfcc_df(args[0]), options.model))
 
 if __name__ == '__main__':
     sys.exit(main())
