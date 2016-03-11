e20b82854e9f500a456fdd4aeae160e6ba060821
Yacin Nadji <yacin@gatech.edu>
2016-03-11 13:18:23 -0500

    output confusion matrix

--- deeply/python/classify_audio.py
+++ deeply/python/classify_audio.py
@@ -10,12 +10,12 @@ from sklearn import svm
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import normalize
+from sklearn.metrics import confusion_matrix
 
 models = {'svm': svm.SVC(kernel='linear', C=1),
           'sgd': SGDClassifier(loss="hinge", penalty="l2"),
           'rf':  RandomForestClassifier(),
 }
-
 
 def _read_mfcc_df(csvpath):
     return pd.read_csv(csvpath,
@@ -32,6 +32,18 @@ def xfold(df, classifier, k=10):
     scores = cross_validation.cross_val_score(clf, X, y, cv=k)
     return scores
 
+def _confusion_matrix(df, classifier):
+    X = df[['mfcc%d' % x for x in range(13)]]
+    normalize(X, copy=False)
+    y = df['class']
+    labels = list(np.unique(y))
+
+    clf = models[classifier]
+    clf.fit(X, y)
+    pred_y = clf.predict(X)
+
+    return confusion_matrix(y, pred_y, labels=labels), labels
+
 def main():
     """main function for standalone usage"""
     usage = "usage: %prog [options] mfcc.csv"
@@ -46,7 +58,12 @@ def main():
         return 2
 
     # do stuff
-    print(xfold(_read_mfcc_df(args[0]), options.model))
+    df = _read_mfcc_df(args[0])
+    print(xfold(df, options.model))
+    cmatrix, labels = _confusion_matrix(df, options.model)
+    print('%s%s' % (' ' * 15, ' '.join(labels)))
+    for label, row in zip(labels, cmatrix):
+        print('%15s %s' % (label, str(row)))
 
 if __name__ == '__main__':
     sys.exit(main())
